2dc8dbe56902d97193e2989d5c3eccb25a4363fc
Stream <streamer@octo>
2025-10-12 12:28:49 +0100

    * Added README * Restructured nob.c

--- nob.c
+++ nob.c
@@ -11,34 +11,16 @@
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
-typedef struct {
-    char ** items;
-    size_t count;
-    size_t capacity;
-} DaStrings;
+typedef struct da_strings_t DaStrings;
+
+void * malloc0 (size_t size);
+char * strip_extention (char * file_name);
+char * c_to_o_ext (char * str);
+DaStrings * compile_files (char * source_files[], size_t len, Nob_Cmd * cmd, Nob_Procs * procs);
+void link_files (DaStrings * object_files,char * exec_name, Nob_Cmd * cmd);
+void clean_files (char * source_files[], size_t len, Nob_Cmd * cmd);
 
 #define EXEC_NAME "nobber"
-
-//#define free0(ptr) \
-//    free (ptr); \
-//    ptr = NULL;
-#define free0(ptr) \
-    memset ((void *)ptr, 0, sizeof ((void *) ptr)); \
-    free (ptr);
-
-#define da_free_items(Type, da) { \
-    for (Type *it = (da)->items; it < (da)->items + (da)->capacity; it++) { \
-        if (*it == NULL) break;\
-        free (*it);\
-    }\
-    free ((da)->items);\
-}
-
-void * malloc0 (size_t size) {
-    void * out_val = malloc (size);
-    memset (out_val, 0, size);
-    return out_val;
-}
 
 const char * default_build_flags[] = {
     "-std=gnu23",
@@ -62,6 +44,66 @@ const char * src_files[] = {
     NULL,
 };
 #define src_file_count (NOB_ARRAY_LEN(src_files) - 1)
+
+//#define free0(ptr) \
+//    free (ptr); \
+//    ptr = NULL;
+#define free0(ptr) \
+    memset ((void *)ptr, 0, sizeof ((void *) ptr)); \
+    free (ptr);
+
+struct da_strings_t {
+    char ** items;
+    size_t count;
+    size_t capacity;
+};
+
+/** 
+ * free the items in a list propperly!
+ * TODO: Need a version that takes a FreeFunc.
+ **/
+#define da_free_items(Type, da) { \
+    for (Type *it = (da)->items; it < (da)->items + (da)->capacity; it++) { \
+        if (*it == NULL) break;\
+        free (*it);\
+    }\
+    free ((da)->items);\
+}
+
+int main (int argc, char ** argv) {
+
+    Nob_Procs procs = {0};
+    Nob_Cmd cmd = {0};
+
+    NOB_GO_REBUILD_URSELF (argc, argv);
+    [[maybe_unused]]  const char * program_name = nob_shift_args(&argc, &argv);
+    const char * command_name = "build";
+    if (argc > 0) command_name = nob_shift_args(&argc, &argv);
+
+    if ((strcmp (command_name, "build")) == 0) {
+        DaStrings * objects = compile_files ((char **)src_files, src_file_count, &cmd, &procs);
+        nob_procs_wait_and_reset (&procs);
+        link_files (objects, EXEC_NAME, &cmd);
+        da_free_items (char *, objects);
+        free (objects);
+    } else if (strcmp (command_name, "clean") == 0) {
+        clean_files ((char**)src_files, src_file_count, &cmd);
+    } else {
+        nob_log (NOB_INFO, "Unknown command %s\n", command_name);
+    }
+    if (cmd.capacity) nob_cmd_free (cmd);
+
+    return 0;
+}
+
+/* ----------------- */
+
+void * malloc0 (size_t size) {
+    void * out_val = malloc (size);
+    memset (out_val, 0, size);
+    return out_val;
+}
+
 
 // https://stackoverflow.com/questions/43163677/how-do-i-strip-a-file-extension-from-a-string-in-c
 char * strip_extention (char * file_name) {
@@ -139,31 +181,5 @@ void clean_files (char * source_files[], size_t len, Nob_Cmd * cmd) {
     nob_cmd_append (cmd, EXEC_NAME);
     nob_cmd_run (cmd);
     da_free_items (char *, &tmp_strings);
-}
-
-int main (int argc, char ** argv) {
-
-    Nob_Procs procs = {0};
-    Nob_Cmd cmd = {0};
-
-    NOB_GO_REBUILD_URSELF (argc, argv);
-    [[maybe_unused]]  const char * program_name = nob_shift_args(&argc, &argv);
-    const char * command_name = "build";
-    if (argc > 0) command_name = nob_shift_args(&argc, &argv);
-
-    if ((strcmp (command_name, "build")) == 0) {
-        DaStrings * objects = compile_files ((char **)src_files, src_file_count, &cmd, &procs);
-        nob_procs_wait_and_reset (&procs);
-        link_files (objects, EXEC_NAME, &cmd);
-        da_free_items (char *, objects);
-        free (objects);
-    } else if (strcmp (command_name, "clean") == 0) {
-        clean_files ((char**)src_files, src_file_count, &cmd);
-    } else {
-        nob_log (NOB_INFO, "Unknown command %s\n", command_name);
-    }
-    if (cmd.capacity) nob_cmd_free (cmd);
-
-    return 0;
 }
 
